d0efe5363ad501991969629e2b59c2ec427ecd3e
pkellner <peter@peterkellner.net>
2016-11-15 22:07:56 -0800

    working with body sending javascript after script tag helper run

--- src/ScriptManagerTagHelper.WebSample/TagHelpers/BodyTagHelper.cs
+++ src/ScriptManagerTagHelper.WebSample/TagHelpers/BodyTagHelper.cs
@@ -12,7 +12,7 @@ namespace ScriptManagerTagHelper.WebSample.TagHelpers
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
 
-    
+
     public class BodyTagHelper : TagHelper
     {
         public override int Order => int.MaxValue - 100;
@@ -22,7 +22,7 @@ namespace ScriptManagerTagHelper.WebSample.TagHelpers
         private readonly IOptions<ScriptManagerOptions> _scriptManagerOptions;
         private readonly IHostingEnvironment _hostingEnvironment;
 
-      
+
 
 
         public BodyTagHelper(IScriptManager scriptManager,
@@ -34,75 +34,19 @@ namespace ScriptManagerTagHelper.WebSample.TagHelpers
             _hostingEnvironment = hostingEnvironment;
         }
 
-        
-
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            var bodyContent = output.Content.IsModified
-                ? output.Content.GetContent()
-                : (await output.GetChildContentAsync()).GetContent();
-
-            var updatedBodyContent = bodyContent;
+            (await output.GetChildContentAsync()).GetContent();
 
             StringBuilder sb = new StringBuilder();
             if (_scriptManager.Scripts.Count > 0)
             {
-                foreach (var scriptRef in _scriptManager.Scripts)
+                foreach (var scriptRef in _scriptManager.Scripts.OrderBy(a => a.IncludeOrderPriorty))
                 {
-                    sb.AppendLine(string.Format("<script src='{0}' ></script>", scriptRef));
+                    sb.AppendLine(string.Format("<script src='{0}' ></script>", scriptRef.ScriptPath));
                 }
-                updatedBodyContent += sb;
             }
-            output.Content.SetHtmlContent(updatedBodyContent);
-
-
-
-            // hostingEnvironment gets me path to file system file
-            // example of how to get files: https://github.com/aspnet/live.asp.net/blob/dev/src/live.asp.net/TagHelpers/ScriptInliningTagHelper.cs
-
-            // from string, need to figure out if it's http type or file system type,
-            // don't minimizie if not file system type
-
-            //                        // update html
-            //                        StringBuilder sb = new StringBuilder();
-            //                        if (_scriptManager.Scripts.Count > 0)
-            //                        {
-            //                            foreach (var scriptRef in _scriptManager.Scripts)
-            //                            {
-            //                                sb.AppendLine(string.Format("<script src='{0}' ></script>", scriptRef));
-            //                            }
-            //                            if (sb.Length > 0)
-            //                            {
-            //                                responseBody = responseBody.Replace("</body>", sb + "</body>");
-            //                            }
-            //                        }
-
-
-            //var sb = new StringBuilder();
-            //foreach (var scriptReference in _scriptManager.Scripts.OrderBy(a => a.IncludeOrderPriorty))
-            //{
-            //    sb.AppendLine($"<script type='text/javascript' src='{scriptReference.ScriptPath}'></script>");
-            //}
-            //output.Content.AppendHtml(sb.ToString());
-
-            //output.PostContent.SetContent(sb.ToString());
-
-            //output.PostContent.SetHtmlContent(sb.ToString());
-
-
-            //var combine = _scriptManagerOptions.Value.CombinedSrc;
-
-            //var combinedScriptTexts = _scriptManager.ScriptTexts;
-
-            // output filenames first, then combine in script tag the combined scripttexts
-            // .Append (return multiple times so OK to return without variable, resharper area)
-            //output.PostContent.Append("this is all the script tags with ...");
-
-            // NOTE: make sure to add CRC cache breaker on javascripts
-
-
-
-
+            output.PostContent.AppendHtml(sb.ToString());
         }
     }
 }
--- src/ScriptManagerTagHelper.WebSample/TagHelpers/ScriptTagHelper.cs
+++ src/ScriptManagerTagHelper.WebSample/TagHelpers/ScriptTagHelper.cs
@@ -8,14 +8,14 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace ScriptManagerTagHelper.WebSample.TagHelpers
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
-    [HtmlTargetElement("script",Attributes = "src")]
+    [HtmlTargetElement("script", Attributes = "src")]
     public class ScriptTagHelper : TagHelper
     {
         private readonly IScriptManager _scriptManager;
 
 
-        // case insenstative
-        // DON'T USE THIS BECAUSE PREVIOUS TAGHELPER RAN ON IT ~ TAG HELPER, MVC SCRIPT TAG HELPER  
+        // src is case insenstative
+        // DON'T SRC USE THIS BECAUSE PREVIOUS TAGHELPER RAN ON IT ~ TAG HELPER, MVC SCRIPT TAG HELPER  
         //public string Src { get; set; }
 
         public ScriptTagHelper(IScriptManager scriptManager)
@@ -27,36 +27,12 @@ namespace ScriptManagerTagHelper.WebSample.TagHelpers
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
+            // needed because of builtin tag helper that looks at src
             var src = output.Attributes["src"].Value.ToString();
             _scriptManager.AddScript(new ScriptReference(src, Convert.ToInt32(IncludeOrderPriority)));
 
-            var childContent = await output.GetChildContentAsync();
+            await output.GetChildContentAsync();
             output.SuppressOutput();
-            
-
-            // DID BODY NOW, NEED TO DO THIS ONE
-            // var childContent = output.Content.is
-           // var src = output.Attributes["src"].Value.ToString(); // get here and not property because other tag helpers might have run
-            //                                                     // src is going to look /foo/abc.js
-
-            // find file passe in to source
-
-
-            // add file to script manager (de-duplicate in script manager)
-
-            //var xx = output.Attributes["IncludeOrderPriority"].ToString();
-
-            //_scriptManager.AddScript(new ScriptReference(src, Convert.ToInt32(IncludeOrderPriority)));
-
-           // await output.SuppressOutput();
-
-            //var childContent = output.Content.IsModified
-            //    ? output.Content.GetContent()
-            //    : (await output.GetChildContentAsync()).GetContent();
-
-            // suppress output
-            //output.SuppressOutput();
-
         }
     }
 }
